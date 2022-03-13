0acbcdbea41b7849333b96d2aaa9226cd46361b7
Jinyang Tang <jit098@ucsd.edu>
2022-03-13 19:58:39 -0400

    update

--- tragent_ios/tragent_ios/ContentView.swift
+++ tragent_ios/tragent_ios/ContentView.swift
@@ -9,13 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Tragent")
+            .font(.title)
+            .fontWeight(.heavy)
+            .foregroundColor(Color.gray)
+            .lineLimit(nil)
             .padding()
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        Group {
+            ContentView()
+                
+        }
     }
 }
